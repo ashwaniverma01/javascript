af5f6b9d5872a2df46a2a67c056454cef25aab5a
ashwaniverma01 <155729001+ashwaniverma01@users.noreply.github.com>
2025-02-26 18:34:28 +0000

    Variable

---
+++ basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = "123"; // Constant variable
+let name = "xyz";        // Mutable variable
+let age = 19;            // Using let instead of var
+let email = "abc@gmail.com"; // Explicitly declaring email
+
+age = 21; // Reassigning age is fine since it's declared with let
+
+console.table([accountId, name, age, email]);
