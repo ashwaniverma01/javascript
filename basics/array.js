201fd7635b22252893aba8a3ff38b6885101b88e
ashwaniverma01 <155729001+ashwaniverma01@users.noreply.github.com>
2025-03-02 11:26:02 +0000

    array

---
+++ basics/array.js
@@ -0,0 +1,22 @@
+// const myarr=[1,2,3,4,5]
+// myarr.push(6)
+// console.log(myarr);
+// myarr.pop(4)
+// console.log("pop");
+// console.log(myarr);
+// console.log("unshift");
+// myarr.unshift(8)
+// console.log(myarr);
+// console.log("shift");
+// myarr.shift()
+// console.log(myarr);
+// const newarr=myarr.join()
+// console.log(newarr);
+// console.log("slice");
+// console.log(myarr.slice(1,3));
+// console.log("splice");
+// console.log(myarr.splice(1,3));
+const myarr=[2,3,4,5,6,7]
+const newarr=["ironman","spiderman","thor"]
+myarr.push(newarr);
+console.log(myarr);
